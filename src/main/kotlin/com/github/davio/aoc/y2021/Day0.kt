a5b587ac47375056a90c36ce47e367c78874dac6
Dave Franken <dwfranken@gmail.com>
2021-12-09 12:30:02 +0100

    Completed day 9

---
+++ src/main/kotlin/com/github/davio/aoc/y2021/Day0.kt
@@ -0,0 +1,27 @@
+package com.github.davio.aoc.y2021
+
+import com.github.davio.aoc.general.call
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    Day0.getResultPart1()
+    measureTimeMillis {
+        Day0.getResultPart2()
+    }.call { println("Took $it ms") }
+}
+
+object Day0 {
+
+    /*
+    */
+
+    fun getResultPart1() {
+
+    }
+
+    /*
+    */
+
+    fun getResultPart2() {
+    }
+}
