babe4a72b3ddd3ca6b6d0fef999188b45a360a87
Dave Franken <dwfranken@gmail.com>
2022-12-12 09:32:20 +0100

    Solved day 12

---
+++ src/main/kotlin/com/github/davio/aoc/y2022/Day12.kt
@@ -0,0 +1,113 @@
+package com.github.davio.aoc.y2022
+
+import com.github.davio.aoc.general.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+//    println(Day12.getResultPart1())
+    measureTimeMillis {
+        println(Day12.getResultPart2())
+    }.call { println("Took $it ms") }
+}
+
+/**
+ * See [Advent of Code 2022 Day 12](https://adventofcode.com/2022/day/12#part2])
+ */
+object Day12 {
+
+    fun getResultPart1(): Int {
+        val lines = getInputAsList()
+        val grid = CharGrid(lines.size) { index -> lines[index].toCharArray() }
+        val startingPoint = grid.getPoints().first { p -> grid.getValue(p) == 'S' }
+        val endPoint = grid.getPoints().first { p -> grid.getValue(p) == 'E' }
+        val route = aStar(grid, startingPoint, endPoint)
+        printRoute(grid, route)
+        return route.size - 1
+    }
+
+    fun getResultPart2(): Int {
+        val lines = getInputAsList()
+        val grid = CharGrid(lines.size) { index -> lines[index].toCharArray() }
+        val startingPoints = grid.getPoints().filter { p -> grid.getValue(p) == 'S' || grid.getValue(p) == 'a' }
+        val endPoint = grid.getPoints().first { p -> grid.getValue(p) == 'E' }
+        val shortestRoute = startingPoints.map { aStar(grid, it, endPoint) }.filter { it.isNotEmpty() }.minBy { it.size }
+        printRoute(grid, shortestRoute)
+        return shortestRoute.size - 1
+    }
+
+    private fun printRoute(grid: CharGrid, route: List<Point>) {
+        val boldRedColor = "\u001B[31m\u001B[1m"
+        val ansiReset = "\u001B[0m"
+
+        grid.getPoints().forEach { p ->
+            val ansiColor = if (route.any { it == Point(p.x, p.y) }) boldRedColor else ansiReset
+            print("$ansiColor${grid.getValue(p)}")
+            if (p.x == grid[0].lastIndex) {
+                println()
+            }
+        }
+    }
+
+
+    private fun reconstructPath(cameFrom: Map<Point, Point>, current: Point): List<Point> {
+        val totalPath = mutableListOf(current)
+        var myCurrent = current
+        while (myCurrent in cameFrom.keys) {
+            myCurrent = cameFrom[myCurrent]!!
+            totalPath.add(0, myCurrent)
+        }
+
+        return totalPath
+    }
+
+    private fun heuristicFunction(point: Point, goal: Point): Int {
+        return (goal.y - point.y) + (goal.x - point.x)
+    }
+
+    private fun aStar(grid: CharGrid, start: Point, goal: Point): List<Point> {
+        // The set of discovered nodes that may need to be (re-)expanded.
+        // Initially, only the start node is known.
+        // This is usually implemented as a min-heap or priority queue rather than a hash-set.
+        val openSet = hashSetOf(start)
+
+        // For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start
+        // to n currently known.
+        val cameFrom = mutableMapOf<Point, Point>()
+
+        // For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
+        val gScore = mutableMapOf(start to 0).withDefault { Int.MAX_VALUE }
+
+        // For node n, fScore[n] := gScore[n] + h(n). fScore[n] represents our current best guess as to
+        // how short a path from start to finish can be if it goes through n.
+        val fScore = mutableMapOf(start to heuristicFunction(start, goal)).withDefault { Int.MAX_VALUE }
+
+        while (openSet.isNotEmpty()) {
+            // This operation can occur in O(1) time if openSet is a min-heap or a priority queue
+            val current = openSet.minByOrNull { node -> fScore.getValue(node) }!!
+            if (current == goal) {
+                return reconstructPath(cameFrom, current)
+            }
+
+            openSet.remove(current)
+            grid.getOrthogonallyAdjacentPoints(current).filter {
+                getCharValue(grid.getValue(it)) - getCharValue(grid.getValue(current)) <= 1
+            }.forEach { neighbor ->
+                // d(current,neighbor) is the weight of the edge from current to neighbor
+                // tentative_gScore is the distance from start to the neighbor through current
+                val dCurrentNeighbor = 1
+                val tentativeGScore = gScore[current]!! + dCurrentNeighbor
+                if (tentativeGScore < gScore.getValue(neighbor)) {
+                    // This path to neighbor is better than any previous one. Record it!
+                    cameFrom[neighbor] = current
+                    gScore[neighbor] = tentativeGScore
+                    fScore[neighbor] = tentativeGScore + heuristicFunction(neighbor, goal)
+                    openSet.add(neighbor)
+                }
+            }
+        }
+
+        return emptyList()
+    }
+
+    private fun getCharValue(c: Char) = if(c == 'S') 'a' else if(c == 'E') 'z' else c
+}
