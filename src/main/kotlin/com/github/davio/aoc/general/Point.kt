37672266f4b41be8ee79bc0cd8d0bac3205496a1
Dave Franken <dwfranken@gmail.com>
2022-12-14 10:23:52 +0100

    Refactored

--- src/main/kotlin/com/github/davio/aoc/general/Point.kt
+++ src/main/kotlin/com/github/davio/aoc/general/Point.kt
@@ -5,15 +5,36 @@ data class Point(val x: Int = 0, val y: Int = 0) : Comparable<Point> {
     operator fun plus(other: Point) = Point(this.x + other.x, this.y + other.y)
     operator fun minus(other: Point) = Point(this.x - other.x, this.y - other.y)
 
-    override fun toString(): String {
-        return "$x,$y"
+    operator fun rangeTo(endInclusive: Point) = sequence {
+        if (x == endInclusive.x) {
+            val firstPair = if (y < endInclusive.y) this@Point else endInclusive
+            val secondPair = if (firstPair == this@Point) endInclusive else this@Point
+            (firstPair.y..secondPair.y).forEach { y ->
+                yield(Point(x, y))
+            }
+        } else if (y == endInclusive.y) {
+            val firstPair = if (x < endInclusive.x) this@Point else endInclusive
+            val secondPair = if (firstPair == this@Point) endInclusive else this@Point
+            (firstPair.x..secondPair.x).forEach { x ->
+                yield(Point(x, y))
+            }
+        } else {
+            val firstPair = if (x < endInclusive.x) this@Point else endInclusive
+            val secondPair = if (firstPair == this@Point) endInclusive else this@Point
+
+            val yInc = if (firstPair.y < secondPair.y) 1 else -1
+            var y = firstPair.y
+            (firstPair.x..secondPair.x).forEach { x ->
+                yield(Point(x, y))
+                y += yInc
+            }
+        }
     }
 
-    override fun compareTo(other: Point): Int {
-        val byX = this.x.compareTo(other.x)
-        if (byX != 0) return byX
-        return this.y.compareTo(other.y)
-    }
+    override fun toString(): String = "$x,$y"
+
+    override fun compareTo(other: Point): Int =
+        Comparator.comparingInt(Point::x).thenComparingInt(Point::y).compare(this, other)
 
     companion object {
         val ZERO = Point(0, 0)
@@ -25,30 +46,3 @@ fun String.toPoint(): Point = this.split(",").let { Point(it[0].toInt(), it[1].t
 typealias Vector = Point
 
 operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>) = Pair(first + other.first, second + other.second)
-
-operator fun Point.rangeTo(endInclusive: Point) = sequence {
-    val startInclusive = this@rangeTo
-    if (startInclusive.x == endInclusive.x) {
-        val firstPair = if (startInclusive.y < endInclusive.y) startInclusive else endInclusive
-        val secondPair = if (firstPair == startInclusive) endInclusive else startInclusive
-        (firstPair.y..secondPair.y).forEach { y ->
-            yield(Point(startInclusive.x, y))
-        }
-    } else if (startInclusive.y == endInclusive.y) {
-        val firstPair = if (startInclusive.x < endInclusive.x) startInclusive else endInclusive
-        val secondPair = if (firstPair == startInclusive) endInclusive else startInclusive
-        (firstPair.x..secondPair.x).forEach { x ->
-            yield(Point(x, startInclusive.y))
-        }
-    } else {
-        val firstPair = if (startInclusive.x < endInclusive.x) startInclusive else endInclusive
-        val secondPair = if (firstPair == startInclusive) endInclusive else startInclusive
-
-        val yInc = if (firstPair.y < secondPair.y) 1 else -1
-        var y = firstPair.y
-        (firstPair.x..secondPair.x).forEach { x ->
-            yield(Point(x, y))
-            y += yInc
-        }
-    }
-}
